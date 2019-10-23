67def4b17165e93158d817259134bfbb1c8e4601
portlek <hsndmrts_@outlook.com>
2019-10-23 15:23:46 +0300

    set, get server

--- src/main/java/com/baharmc/api/Bahar.java
+++ src/main/java/com/baharmc/api/Bahar.java
@@ -1,4 +1,32 @@
 package com.baharmc.api;
 
-public class Bahar {
+public final class Bahar {
+
+    private static Server server;
+
+    /**
+     * Get the global server instance
+     *
+     * @return The server instance
+     */
+    public static Server getServer() {
+        if (server == null)
+            throw new RuntimeException("Server isn't set!");
+
+        return server;
+    }
+
+    /**
+     * Sets the global server instance
+     * <b>Should not be used by plugins!</b>
+     *
+     * @param server Server instance
+     */
+    public static void setServer(Server server) {
+        if (Bahar.server != null)
+            throw new RuntimeException("Server is already set!");
+
+        Bahar.server = server;
+    }
+
 }
--- src/main/java/com/baharmc/api/Server.java
+++ src/main/java/com/baharmc/api/Server.java
@@ -1,4 +1,10 @@
 package com.baharmc.api;
 
+import org.jetbrains.annotations.NotNull;
+
 public interface Server {
+
+    @NotNull
+    String getBrandName();
+
 }

f491d15c3fec3709f426616e187f1a61dbf8fdc5
OnBlock-Dev <47112191+OnBlock@users.noreply.github.com>
2019-11-03 20:47:51 +0330

    Added DisplayBrandName in Server

--- src/main/java/com/baharmc/api/Server.java
+++ src/main/java/com/baharmc/api/Server.java
@@ -1,10 +1,39 @@
 package com.baharmc.api;
 
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.jetbrains.annotations.NotNull;
 
 public interface Server {
 
+    /**
+     * Sets the custom display brand
+     *
+     * @param brandName - custom text to set
+     */
+    void setDisplayBrandName(String brandName);
+
+    /**
+     * Gets the custom display brand
+     *
+     * @return custom brand name
+     */
+    @NotNull
+    String getDisplayBrandName();
+
+    /**
+     * Gets the default Server brand
+     *
+     * @return default Bahar brand name
+     */
     @NotNull
     String getBrandName();
+
+    /**
+     * Gets the version of Bahar server
+     *
+     * @return version
+     */
+    String getVersion();
 
 }
